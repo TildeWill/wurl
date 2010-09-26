cb72307b911d3d46fbaf65f868329c189740dde0
Will Read <will.read@gmail.com>
2010-09-26 02:32:21 -0700

    can't be shuffling files around on heroku (produciton)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
+EnginesHelper.autoload_assets = false
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
