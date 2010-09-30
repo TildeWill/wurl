e027f4f19d60e9ebef65ffc1b10e9c4a805caae6
Will Read <will.read@gmail.com>
2010-09-30 03:39:39 -0700

    show the error instead of blowing up

--- vendor/plugins/whurl/app/controllers/whurl/commands_controller.rb
+++ vendor/plugins/whurl/app/controllers/whurl/commands_controller.rb
@@ -8,13 +8,11 @@ class Whurl::CommandsController < ApplicationController
     @api_url = params.delete("url")
     @method = params.delete("method")
     redirect_to new_whurl_commands_path and return unless @api_url.present?
-
-    command = Whurl::Command.new(@api_url, @method, params)
-
     begin
+      command = Whurl::Command.new(@api_url, @method, params)
       @api_response = command.response_html
-    rescue
-      @api_response = command.body_str
+    rescue Exception => e
+      @api_response = e.message
     end
 
   end
