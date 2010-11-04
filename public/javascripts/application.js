52ee3189fdef2368e2e839fcc07945199fac43a5
Will Read <will.read@gmail.com>
2010-11-03 22:31:19 -0700

    add delete buttons!!!!!!!!!!!!1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,19 +5,27 @@ var Whurl = {
     addHeader: function() {
         var $headerFields = $('.header_pair:last').clone();
         $headerFields.children('input').val("");
+        $headerFields.children('a').css('display', 'inline');
         $headerFields.appendTo('#headers');
 
     },
-    deleteHeader: function() {
-
+    deleteHeader: function(element) {
+        var $paramFields = $(element).closest(".header_pair");
+        $paramFields.slideUp(function() {
+            $paramFields.remove();
+        });
     },
     addParam: function() {
         var $paramFields = $('.param_pair:last').clone();
         $paramFields.children('input').val("");
+        $paramFields.children('a').css('display', 'inline');
         $paramFields.appendTo('#params');
     },
-    deleteParam: function() {
-
+    deleteParam: function(element) {
+        var $paramFields = $(element).closest(".param_pair");
+        $paramFields.slideUp(function() {
+            $paramFields.remove();
+        });
     }
 };
 
@@ -26,8 +34,16 @@ $(document).ready(function() {
         Whurl.addHeader();
     });
 
+    $('.delete_header').live('click', function() {
+        Whurl.deleteHeader(this);
+    });
+
     $('#add_param').click(function() {
         Whurl.addParam();
+    });
+
+    $('.delete_param').live('click', function() {
+        Whurl.deleteParam(this);
     });
 
     $('#permalink').click(function() {
