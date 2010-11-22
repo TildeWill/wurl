c79b03401baa9e9c8c155a35de52658f9e865e33
Will Read <will.read@gmail.com>
2010-11-22 01:52:33 -0600

    provide some default images for the buttons

--- vendor/plugins/whurl/init.rb
+++ vendor/plugins/whurl/init.rb
@@ -1,7 +1,18 @@
-target = File.join(Rails.root, 'public', 'javascripts', 'whurl')
-javascripts = Pathname.new(File.dirname(__FILE__)) + 'public/javascripts'
-FileUtils.mkdir_p(target)
+begin #javascripts
+  target = File.join(Rails.root, 'public', 'javascripts', 'whurl')
+  javascripts = Pathname.new(File.dirname(__FILE__)) + 'public/javascripts'
+  FileUtils.mkdir_p(target)
 
-javascripts.children.each do |file|
-  FileUtils.cp_r(file, File.join(target, file.basename))
+  javascripts.children.each do |file|
+    FileUtils.cp_r(file, File.join(target, file.basename))
+  end
+end
+begin #images
+  target = File.join(Rails.root, 'public', 'images', 'whurl')
+  javascripts = Pathname.new(File.dirname(__FILE__)) + 'public/images'
+  FileUtils.mkdir_p(target)
+
+  javascripts.children.each do |file|
+    FileUtils.cp_r(file, File.join(target, file.basename))
+  end
 end
