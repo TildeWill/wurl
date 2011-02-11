41274f349bc96b3ebdcef0cd07dd60d7503e4bb0
Will Read <will.read@gmail.com>
2011-02-10 20:11:05 -0800

    format error response

--- app/controllers/whurls_controller.rb
+++ app/controllers/whurls_controller.rb
@@ -40,7 +40,7 @@ class WhurlsController < ApplicationController
     end
 
   rescue Exception => e
-    @api_response = e.message + "<br />\n<br />\n" + e.backtrace.join("<br />\n") + client_response
+    @api_response = e.message + "<br />\n<br />\n" + e.backtrace.join("<br />\n") + client_response.body
   end
 end
 
