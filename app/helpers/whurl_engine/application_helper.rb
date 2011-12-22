fb2907516381e0319dda6a3bdb48fce5cbab1c57
Will Read <will.read@gmail.com>
2011-12-21 20:18:31 -0800

    re-enable clippy functionality for sharable link

--- app/helpers/whurl_engine/application_helper.rb
+++ app/helpers/whurl_engine/application_helper.rb
@@ -1,18 +1,19 @@
 module WhurlEngine
   module ApplicationHelper
     def clippy(text, bgcolor='#FFFFFF')
+      swf_path = asset_path('whurl_engine/clippy.swf')
         html = <<-EOF
         <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
                 width="16"
                 height="16"
                 id="clippy" >
-        <param name="movie" value="/flash/whurl_engine/clippy.swf"/>
+        <param name="movie" value="#{swf_path}"/>
         <param name="allowScriptAccess" value="always" />
         <param name="quality" value="high" />
         <param name="scale" value="noscale" />
         <param NAME="FlashVars" value="text=#{text}">
         <param name="bgcolor" value="#{bgcolor}">
-        <embed src="/flash/clippy.swf"
+        <embed src="#{swf_path}"
                width="16"
                height="16"
                name="clippy"
