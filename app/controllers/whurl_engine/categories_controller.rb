f5f64e7173a54266b2e2e456378b7c419e0073ec
Will Read <will.read@gmail.com>
2012-01-06 17:01:36 -0800

    can CRUD categories

--- app/controllers/whurl_engine/categories_controller.rb
+++ app/controllers/whurl_engine/categories_controller.rb
@@ -3,6 +3,11 @@ module WhurlEngine
     def index
       @categories = WhurlEngine::Category.all.sort
     end
+
+    def new
+      @category = WhurlEngine::Category.new
+    end
+
     def create
       @category = WhurlEngine::Category.new(params[:category])
       respond_to do |format|
@@ -14,15 +19,30 @@ module WhurlEngine
       end
     end
 
+    def edit
+      @category = WhurlEngine::Category.find(params[:id])
+    end
+
+    def update
+      @category = WhurlEngine::Category.find(params[:id])
+      respond_to do |format|
+        if @category.update_attributes(params[:category])
+          format.js
+        else
+          format.js { render :partial => 'error' }
+        end
+      end
+    end
+
     def destroy
       @category = WhurlEngine::Category.find(params[:id])
       respond_to do |format|
-      if @category.destroy
-        format.js
-      else
-        format.js { render :partial => 'error' }
+        if @category.destroy
+          format.js
+        else
+          format.js { render :partial => 'error' }
+        end
       end
-    end
     end
   end
 end
